9749b098403e56f35a3c88f9958831b0bf0248d3
Vince <vince@cardinalblue.com>
2012-09-10 14:57:14 +0800

    create PublicImage class for image from internet

---
+++ ComicApp/src/tw/edu/ntu/mobilehero/view/PublicImage.java
@@ -0,0 +1,39 @@
+package tw.edu.ntu.mobilehero.view;
+
+public class PublicImage {
+    private String mThumbUrl;
+    private String mOrigUrl;
+    private String mIndex;
+    private String mViewer;
+    private String mLikedNum;
+    public String getmThumbUrl() {
+        return mThumbUrl;
+    }
+    public void setmThumbUrl(String mThumbUrl) {
+        this.mThumbUrl = mThumbUrl;
+    }
+    public String getmOrigUrl() {
+        return mOrigUrl;
+    }
+    public void setmOrigUrl(String mOrigUrl) {
+        this.mOrigUrl = mOrigUrl;
+    }
+    public String getmIndex() {
+        return mIndex;
+    }
+    public void setmIndex(String mIndex) {
+        this.mIndex = mIndex;
+    }
+    public String getmViewer() {
+        return mViewer;
+    }
+    public void setmViewer(String mViewer) {
+        this.mViewer = mViewer;
+    }
+    public String getmLikedNum() {
+        return mLikedNum;
+    }
+    public void setmLikedNum(String mLikedNum) {
+        this.mLikedNum = mLikedNum;
+    }
+}
